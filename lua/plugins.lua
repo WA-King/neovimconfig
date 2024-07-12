db92a368e8cbbd51178558a7c33dee055a4276b1
liuchengwen <403567557@qq.com>
2024-07-12 15:50:25 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,19 +15,41 @@ require("lazy").setup({
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
     {"folke/tokyonight.nvim",lazy = false,priority = 1000,opts = {}},
     {
-	'nvim-lualine/lualine.nvim',
+        'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
     },
     {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
     'mhartington/formatter.nvim',
     {
-	'hrsh7th/nvim-cmp',
+        'hrsh7th/nvim-cmp',
         dependencies = { 
-	    'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-nvim-lua',
             'hrsh7th/cmp-path',
         },
     },
     'phaazon/hop.nvim',
     'neovim/nvim-lspconfig',
-})
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+    'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    {
+        "L3MON4D3/LuaSnip",
+            -- follow latest release.
+            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp"
+    },
+    {
+     "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {},
+        config = function(_, opts) require'lsp_signature'.setup(opts) end
+        }
+}
