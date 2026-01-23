bc4bbcabdf490503884aebf8fdf5ada2f82c30d0
liuchengwen <44970685+WA-King@users.noreply.github.com>
2026-01-23 14:32:35 +0800

    Update LSP diagnostics config

--- lua/plugin_setting/lspconfig.lua
+++ lua/plugin_setting/lspconfig.lua
@@ -6,22 +6,18 @@ local nvim_lsp = require('lspconfig')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
+local on_attach = function(_, bufnr)
     -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Mappings.
-    local opts = { noremap=true, silent=true }
+    local opts = { noremap = true, silent = true, buffer = bufnr }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
@@ -47,20 +43,12 @@ require'lspconfig'.cmake.setup {
     cmd = { vim.fn.stdpath('data')..'/lsp_servers/cmake/venv/bin/cmake-language-server' },
 }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    -- This sets the spacing and the prefix, obviously.
-    virtual_text = {
-      spacing = 4,
-      prefix = '',
-      source = "always"
-    }
-  }
-)
-
 vim.diagnostic.config({
-  virtual_text = true,
+  virtual_text = {
+    spacing = 4,
+    prefix = '',
+    source = "always",
+  },
   signs = true,
   underline = true,
   update_in_insert = false,
